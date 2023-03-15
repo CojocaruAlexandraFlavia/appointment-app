f73d7d1b965d10ae7f41261e703e8ca0ab08e138
Alexandra Cojocaru <alexandraflavia00@gmal.com>
2023-03-16 01:10:50 +0200

    Started login component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Login from "./src/components/Login";
+import { NativeBaseProvider } from "native-base";
+import { LoginFormProvider } from "./src/store/LoginForm.context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+        <LoginFormProvider>
+            <Login/>
+        </LoginFormProvider> 
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
