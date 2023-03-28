d78931a9a01f8f1700547d833c183230c71a82a9
Alexandra Cojocaru <alexandraflavia00@gmal.com>
2023-03-28 22:16:57 +0300

    Added home client content

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -111,4 +111,27 @@ export default function HomeClient({navigation}: any) {
         //
         // </Drawer.Navigator>
     // );
+
+    const [mockData, setMockData] = useState<Salon[]>(salons)
+
+    return (
+        <Center w="100%">
+            <Box safeArea p="2" py="8" w="100%" maxW="290">
+                <Heading size={"lg"} mb={4} alignSelf={"center"}>Salons</Heading>
+                <FlatList data={mockData} renderItem={({item}) =>
+                    <Pressable onPress={() => navigation.navigate('Salon', {id: item.id})}>
+                        <Box borderBottomWidth="1" _dark={{ borderColor: "muted.50"}} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
+                            <HStack space={"sm"}>
+                                <Avatar size="48px" source={{uri: item.images[0]}} mr={7}/>
+                                <VStack>
+                                    <Heading style={{alignSelf:"center", fontSize:20}}>{item.name}</Heading>
+                                    <Rating type="custom" startingValue={item.rating}  imageSize={25} readonly />
+                                </VStack>
+                            </HStack>
+                        </Box>
+                    </Pressable> }>
+                </FlatList>
+            </Box>
+        </Center>
+    )
 }
